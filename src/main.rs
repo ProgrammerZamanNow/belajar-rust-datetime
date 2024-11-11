c7e186f14f141461721e58111e543a32e78d65a9
khannedy <echo.khannedy@gmail.com>
2024-11-11 21:03:56 +0700

    Add test for NaiveDateTime in tests module
    This commit introduces a new test function, `test_date_time`, to validate the `NaiveDateTime` functionality of the `chrono` crate. Additionally, the `NaiveDateTime` import has been added to the list of imports from the `chrono` library to support this test. The test verifies year, month, day, hour, minute, second, and nanosecond components of the `NaiveDateTime`.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use chrono::{Datelike, Duration, NaiveDate, NaiveTime, Timelike};
+    use chrono::{Datelike, Duration, NaiveDate, NaiveDateTime, NaiveTime, Timelike};
 
     #[test]
     fn test_date(){
@@ -33,5 +33,21 @@ mod tests {
         println!("{}", time.minute());
         println!("{}", time.second());
         println!("{}", time.nanosecond());
+    }
+
+    #[test]
+    fn test_date_time(){
+        let date_time: NaiveDateTime = NaiveDateTime::new(
+            NaiveDate::from_ymd_opt(2024, 10, 10).unwrap(),
+            NaiveTime::from_hms_opt(12, 12, 12).unwrap()
+        );
+
+        println!("{}", date_time.date().year());
+        println!("{}", date_time.date().month());
+        println!("{}", date_time.date().day());
+        println!("{}", date_time.time().hour());
+        println!("{}", date_time.time().minute());
+        println!("{}", date_time.time().second());
+        println!("{}", date_time.time().nanosecond());
     }
 }
