3e06c68d342933ef1566ee188a4f26923fa6a147
khannedy <echo.khannedy@gmail.com>
2024-11-11 21:15:24 +0700

    Add chrono-tz dependency and timezone-related tests
    Added the `chrono-tz` crate to the project dependencies for handling time zones. Implemented tests for offset handling and time zone conversions, specifically demonstrating usage with Jakarta timezone.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,8 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use chrono::{Datelike, Duration, NaiveDate, NaiveDateTime, NaiveTime, Timelike};
+    use chrono::{Datelike, Duration, FixedOffset, NaiveDate, NaiveDateTime, NaiveTime, TimeZone, Timelike};
+    use chrono_tz::Asia;
 
     #[test]
     fn test_date(){
@@ -49,5 +50,33 @@ mod tests {
         println!("{}", date_time.time().minute());
         println!("{}", date_time.time().second());
         println!("{}", date_time.time().nanosecond());
+    }
+
+    #[test]
+    fn test_offset(){
+        let utc_date_time: NaiveDateTime = NaiveDateTime::new(
+            NaiveDate::from_ymd_opt(2024, 10, 30).unwrap(),
+            NaiveTime::from_hms_opt(10, 30, 0).unwrap()
+        );
+
+        let asia_jakarta: FixedOffset = FixedOffset::east_opt(7 * 3600).unwrap();
+        let asia_jakarta_date_time = asia_jakarta.from_utc_datetime(&utc_date_time);
+
+        println!("{}", utc_date_time);
+        println!("{}", asia_jakarta_date_time);
+    }
+
+    #[test]
+    fn test_time_zone(){
+        let utc_date_time: NaiveDateTime = NaiveDateTime::new(
+            NaiveDate::from_ymd_opt(2024, 10, 30).unwrap(),
+            NaiveTime::from_hms_opt(10, 30, 0).unwrap()
+        );
+
+        let asia_jakarta = Asia::Jakarta;
+        let asia_jakarta_date_time = asia_jakarta.from_utc_datetime(&utc_date_time);
+
+        println!("{}", utc_date_time);
+        println!("{}", asia_jakarta_date_time);
     }
 }
