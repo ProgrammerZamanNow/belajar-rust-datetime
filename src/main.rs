fd4d086daa06e63273083b2ebf698cd4bc01c4b2
khannedy <echo.khannedy@gmail.com>
2024-11-11 20:54:06 +0700

    Add duration test case
    This commit adds a new test case to verify date manipulation using the `Duration` feature from the `chrono` crate. Additionally, the `Duration` module is now imported to support this functionality. The existing test case remains unchanged.

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use chrono::{Datelike, NaiveDate};
+    use chrono::{Datelike, Duration, NaiveDate};
 
     #[test]
     fn test_date(){
@@ -13,5 +13,15 @@ mod tests {
         println!("{}", date.year());
         println!("{}", date.month());
         println!("{}", date.day());
+    }
+
+    #[test]
+    fn test_duration(){
+        let date: NaiveDate = NaiveDate::from_ymd_opt(2024, 10, 25).unwrap();
+        let duration: Duration = Duration::days(10);
+        let new_date: NaiveDate = date - duration;
+
+        println!("{}", date);
+        println!("{}", new_date);
     }
 }
