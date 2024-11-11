2dfd1114f0cf4924ff42ee55957c78f5b5defc99
khannedy <echo.khannedy@gmail.com>
2024-11-11 20:46:06 +0700

    Add unit test for date formatting
    This commit introduces a test module within `main.rs` to verify date formatting, using the `chrono` crate. It specifically tests the year, month, and day extraction from a `NaiveDate` instance. This ensures correctness of date-related operations in the code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use chrono::{Datelike, NaiveDate};
+
+    #[test]
+    fn test_date(){
+        let date: NaiveDate = NaiveDate::from_ymd_opt(2024, 10, 25).unwrap();
+
+        println!("{}", date.year());
+        println!("{}", date.month());
+        println!("{}", date.day());
+    }
+}
